46bd5632f7c07609f4c67074e4f7ba0263a2e62b
Oleg <MaRhAl_161@bk.ru>
2025-02-17 00:27:46 -0800

    Create application Biology App v 0.0.1

---
+++ biology_app/README.md
@@ -0,0 +1,3 @@
+# biology_app
+
+A new Flutter project.
---
+++ biology_app/android/app/src/main/kotlin/com/example/biology_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.biology_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
